b4200c59ee3794fc6e6fd99220faf2d1a7eae835
andrew <andrew>
2000-03-18 02:51:32 +0000

    Implementing into production.

--- scripts/get-latest.sh
+++ scripts/get-latest.sh
@@ -6,8 +6,8 @@ SITE=$1
 [ "$SITE" == "" ] && SITE=socrates.catalyst.net.nz
 
 echo "Grabbing latest version off $SITE"
-ssh $SITE 'cd ~/wrms/scripts; ./dump-requests.sh'
-scp $SITE:wrms/scripts/dumped* ~/wrms/scripts
+ssh $SITE 'cd ~/wrms/scripts; ./dump-db.sh'
+scp $SITE:wrms/scripts/dump/* ~/wrms/scripts/dump
 pushd ~/wrms/scripts
 ./recreate-wrms.sh
 popd

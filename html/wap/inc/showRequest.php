1c0bf4ac5be1fa0def43d8b3d0716983df6b60b1
andrew <andrew>
2000-09-05 01:42:40 +0000

    Fix query idiocy.

--- html/wap/inc/showRequest.php
+++ html/wap/inc/showRequest.php
@@ -6,9 +6,9 @@
   $request = "<p><small>";
   if(isset($id)) {
 
-    $query = "SELECT DISTINCT ON request_id request.request_id, detailed, severity_code, 
-		request_by, urgency, last_activity, lookup_desc AS status_desc
-		FROM request, request_interested, lookup_code AS status, usr 
+    $query = "SELECT request_id, detailed, severity_code, 
+		request_by, urgency
+		FROM request 
 		WHERE request.request_id=$id";
 
     $result = pg_Exec( $wrms_db, $query );
@@ -16,12 +16,13 @@
     for ( $i=0; $i < pg_NumRows($result); $i++ ) {
       $thisrequest = pg_Fetch_Object( $result, $i );
       $request .= tidy($thisrequest->detailed)."<br/>
-		Request By: ".tidy($thisrequest->request_by)."<br/>
-		Severity Code: ".tidy($thisrequest->severity_code)."<br/>
-		Urgency: ".tidy($thisrequest->urgency);
+      Request By: ".tidy($thisrequest->request_by)."<br/>
+      Severity Code: ".tidy($thisrequest->severity_code)."<br/>
+      Urgency: ".tidy($thisrequest->urgency);
     }
-  } else {
-	$request .= "I'm sorry the request id you gave was invalid";
+  }
+  else {
+    $request .= "I'm sorry the request id you gave was invalid";
   }
   $request .= "</small></p>";
 

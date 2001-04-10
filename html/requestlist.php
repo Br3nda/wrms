2d083a491c17749cee554e9f72e78269c5edee0a
gavin <gavin>
2001-04-10 04:09:33 +0000

    Added Activity from and to date search criteria.

--- html/requestlist.php
+++ html/requestlist.php
@@ -25,7 +25,19 @@
 <table border=0 cellspacing=0 cellpadding=2 align=center bgcolor=<?php echo $colors[6]; ?>>
 <tr valign=middle>
 <td class=smb align=right>Search:</td><td class=sml><font size=1><input class=sml TYPE="Text" Size="10" Name="search_for" Value="<?php echo "$search_for"; ?>"></font></td>
-<td class=smb align=right>&nbsp; System:</td><td class=sml><font size=1><select class=sml name=system_code><option value=".">All Systems</option><?php echo "$system_list"; ?></select></font></td>
+<td class=smb align=right>&nbsp;System:</td><td class=sml><font size=1><select class=sml name=system_code><option value=".">All Systems</option><?php echo "$system_list"; ?></select></font></td>
+
+<td class=smb align=right>Last&nbsp;Activity&nbsp;From:</td>
+<td nowrap class=smb><input type=text size=10 name=from_date class=sml value="<?php echo "$from_date"; ?>">
+<a href="javascript:show_calendar('forms[0].from_date');" onmouseover="window.status='Date Picker';return true;" onmouseout="window.status='';return true;"><img valign="middle" src="/images/date-picker.gif" border=0></a>
+</td>
+
+
+<td class=smb align=right>&nbsp;To:</td>
+<td nowrap class=smb><input type=text size=10 name=to_date class=sml value="<?php echo "$to_date"; ?>">
+<a href="javascript:show_calendar('forms[0].to_date');" onmouseover="window.status='Date Picker';return true;" onmouseout="window.status='';return true;"><img valign="middle" src="/images/date-picker.gif" border=0></a>
+</td>
+
 <td rowspan=2 valign=middle class=sml>&nbsp;<BR><input TYPE=Image src=images/in-go.gif alt=go WIDTH=44 BORDER=0 HEIGHT=26 name=submit></td></tr>
 <?php
   $query = "SELECT * FROM lookup_code WHERE source_table='request' ";
@@ -36,7 +48,7 @@
     echo "<p>$query";
   }
   else if ( pg_NumRows($rid) > 1 ) {
-    echo "<tr valign=middle><td class=smb align=right valign=top>Statuses:</td><td colspan=3 class=sml valign=top>\n";
+    echo "<tr valign=middle><td class=smb align=right valign=top>Statuses:</td><td colspan=7 class=sml valign=top>\n";
     for ( $i=0; $i<pg_NumRows($rid); $i++ ) {
       $status = pg_Fetch_Object( $rid, $i );
       echo "<input type=checkbox name=incstat[$status->lookup_code]";
@@ -54,7 +66,7 @@
 
 <?php
   if ( "$search_for$org_code$system_code " != "" ) {
-    $query = "SELECT request_id, brief, fullname, email, lookup_desc AS status_desc FROM request, usr, lookup_code AS status ";
+    $query = "SELECT request_id, brief, fullname, email, lookup_desc AS status_desc, last_activity FROM request, usr, lookup_code AS status ";
 
     $query .= " WHERE request.request_by=usr.username ";
     if ( "$inactive" == "" )        $query .= " AND active ";
@@ -75,6 +87,11 @@
       $query .= " OR detailed ~* '$search_for' ) ";
     }
     if ( "$system_code" != "" )     $query .= " AND system_code='$system_code' ";
+
+    if ( "$from_date" != "" )     $query .= " AND request.last_activity>='$from_date' ";
+
+    if ( "$to_date" != "" )     $query .= " AND request.last_activity<='$to_date' ";
+
     if ( isset( $incstat) ) {
       $query .= " AND (request.last_status ~* '[";
       while( list( $k, $v) = each( $incstat ) ) {
@@ -82,6 +99,8 @@
       }
       $query .= "]') ";
     }
+
+
     $query .= " AND status.source_table='request' AND status.source_field='status_code' AND status.lookup_code=request.last_status ";
     $query .= " ORDER BY request_id DESC ";
     $query .= " LIMIT 100 ";
@@ -95,7 +114,9 @@
       echo "<p>" . pg_NumRows($result) . " requests found</p>"; // <p>$query</p>";
       echo "<table border=\"0\" align=center><tr>\n";
       echo "<th class=cols>WR&nbsp;#</th><th class=cols>Requested By</th>";
-      echo "<th class=cols>Description</th><th class=cols>Status</th><th class=cols>Actions</th></tr>";
+      echo "<th class=cols>Description</th><th class=cols>Status</th><th class=cols>Actions</th>";
+      echo "<th class=cols>Last Activity</th>";
+      echo "</tr>";
 
       // Build table of requests found
       for ( $i=0; $i < pg_NumRows($result); $i++ ) {
@@ -111,6 +132,7 @@
         echo "</a></td>\n";
         echo "<td class=sml>$thisrequest->status_desc</td>\n";
         echo "<td class=sml nowrap>&nbsp;</td>\n";
+        echo "<td class=sml>" . substr(nice_date($thisrequest->last_activity),7) . "</td>\n";
 
         echo "</tr>\n";
       }

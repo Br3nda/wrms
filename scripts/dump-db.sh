2e8dce5abc0380cd0721a671121784be7c2b363a
andrew <andrew>
2001-10-28 18:18:48 +0000

    Dump without wrapping SQL syntax.

--- scripts/dump-db.sh
+++ scripts/dump-db.sh
@@ -8,7 +8,11 @@ pg_dump -s -n -d ${DATABASE} >${DUMPDIR}/schema-dump.sql
 
 for TABLE in `psql -qt -d $DATABASE -c "SELECT relname from pg_class where relkind != 'i' AND relname !~ '^pg_.*' AND relname !~ '^pga.*' AND relname !~ '.*_seq$';"` ; do
   echo "processing $TABLE"
-  pg_dump -a -n -D -d ${DATABASE} -t $TABLE >${DUMPDIR}/t-$TABLE.sql
+  if [ "$TABLE" = "request_history" ]; then
+    pg_dump -a -n -D -d ${DATABASE} -t $TABLE >${DUMPDIR}/t-$TABLE.sql
+  else
+    pg_dump -a -n ${DATABASE} -t $TABLE >${DUMPDIR}/t-$TABLE.sql
+  fi
 done
 
 # Remove the request_words table which is built on load

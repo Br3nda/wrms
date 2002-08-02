97340759c3a11730501557d5591d0e3831d2de12
simon <simon>
2002-08-02 03:45:44 +0000

    Updated get-latest.sh to tar and retrieve the dump files from /tmp/wrms-dump

--- scripts/get-latest.sh
+++ scripts/get-latest.sh
@@ -2,11 +2,12 @@
 
 # Update local database from socrates
 SITE=$1
+DUMPDIR=/tmp/wrms-dump
 
 [ "$SITE" == "" ] && SITE=sartre.catalyst.net.nz
 
 echo "Grabbing latest version off $SITE"
-ssh $SITE 'cd ~/wrms/scripts; ./dump-db.sh; tar cvfz ~/wrms-tables.tgz dump/t-*.sql'
+ssh $SITE 'cd ~/wrms/scripts; ./dump-db.sh; tar cvfz ~/wrms-tables.tgz $DUMPDIR/t-*.sql'
 scp $SITE:wrms-tables.tgz ~/wrms/scripts/dump
 pushd ~/wrms/scripts
 tar xvfz dump/wrms-tables.tgz

8452ea464deec2230f009d43c2fc971eb9b88a3a
simon <simon>
2002-08-05 02:08:44 +0000

    Updated get-latest.sh to remove temporary wrms-tables.tgz from home folder on database hosting machine.

--- scripts/get-latest.sh
+++ scripts/get-latest.sh
@@ -9,7 +9,7 @@ DUMPDIR=/tmp/wrms-dump
 echo "Grabbing latest version off $SITE"
 ssh $SITE "cd ~/wrms/scripts; ./dump-db.sh $DUMPDIR; cd $DUMPDIR; tar cvfz ~/wrms-tables.tgz t-*.sql"
 scp $SITE:wrms-tables.tgz ~/wrms/scripts/dump
-ssh $SITE "cd $DUMPDIR; rm *.sql"
+ssh $SITE "rm wrms-tables.tgz; cd $DUMPDIR; rm *.sql"
 pushd ~/wrms/scripts/dump
 tar xvfz wrms-tables.tgz
 pushd ~/wrms/scripts

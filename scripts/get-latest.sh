d5c3e613f02ec322bda2ae814309c8980e299eee
simon <simon>
2002-08-05 01:45:15 +0000

    Updated get-latest.sh to pass a dump directory argument with its call to dump-db.sh and to also clean up this dump directory after the dump files have been copied across.

--- scripts/get-latest.sh
+++ scripts/get-latest.sh
@@ -7,9 +7,12 @@ DUMPDIR=/tmp/wrms-dump
 [ "$SITE" == "" ] && SITE=sartre.catalyst.net.nz
 
 echo "Grabbing latest version off $SITE"
-ssh $SITE 'cd ~/wrms/scripts; ./dump-db.sh; tar cvfz ~/wrms-tables.tgz $DUMPDIR/t-*.sql'
+ssh $SITE "cd ~/wrms/scripts; ./dump-db.sh $DUMPDIR; cd $DUMPDIR; tar cvfz ~/wrms-tables.tgz t-*.sql"
 scp $SITE:wrms-tables.tgz ~/wrms/scripts/dump
+ssh $SITE "cd $DUMPDIR; rm *.sql"
+pushd ~/wrms/scripts/dump
+tar xvfz wrms-tables.tgz
 pushd ~/wrms/scripts
-tar xvfz dump/wrms-tables.tgz
 ./recreate-wrms.sh
+popd
 popd

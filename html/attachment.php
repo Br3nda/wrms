a831bb1d04bea191ed4eef58902584974595dde7
catalyst <catalyst>
2003-04-15 02:00:04 +0000

    Identify debug messages.

--- html/attachment.php
+++ html/attachment.php
@@ -3,7 +3,7 @@
 
   if ( isset($id) ) $id = intval($id);
   if ( $id == 0 ) {
-    error_log( "attachment: \$id not defined", 0);
+    error_log( "wrms attachment: DBG: \$id not defined", 0);
     echo "<html><head><title>Error - invalid attachment ID</title><body><h1>Invalid attachment ID</h1></body></html>";
     exit;
   }
@@ -22,7 +22,7 @@
     $query .= "WHERE attachment_id = $id ; ";
     $rid = awm_pgexec( $dbconn, $query, "imp");
     if ( !$rid || pg_NumRows($rid) == 0 ) {
-      error_log( "attachment: id [$id] not found", 0);
+      error_log( "wrms attachment: DBG: id [$id] not found", 0);
       echo "<html><head><title>Error - invalid attachment ID [$id]</title><body><h1>Invalid attachment ID [$id]</h1></body></html>";
       exit;
     }
@@ -43,12 +43,12 @@
   $bytes = filesize( "attachments/$id" );
   header("Content-length: $bytes");
   $written = readfile(  "attachments/$id" );
-  if ( $written != $bytes ) error_log( "attachment: Didn't write complete file - $bytes vs. $written sent.", 0);
+  if ( $written != $bytes ) error_log( "wrms attachment: DBG: Didn't write complete file - $bytes vs. $written sent.", 0);
 
   if ( $debuglevel > 0 ) {
     $total_query_time = sprintf( "%3.06lf", $total_query_time );
     $total_time = sprintf( "%3.06lf", duration( $begin_processing, microtime() ));
-    error_log( "attachments cImpleted: TQ=$total_query_time ($total_query_count queries) TT=$total_time URI: $REQU
+    error_log( "wrms attachment: cImpleted: TQ=$total_query_time ($total_query_count queries) TT=$total_time URI: $REQU
 EST_URI", 0);
   }
 ?>

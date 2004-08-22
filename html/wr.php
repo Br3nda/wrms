7914cc8c8696fc0e595e9e93de2090b05184ef22
andrew <andrew>
2004-08-22 02:48:04 +0000

    Start of a new request view/maintain screen.

---
+++ html/wr.php
@@ -0,0 +1,39 @@
+<?php
+require_once("always.php");
+require_once("authorisation-page.php");
+if ( !$session->logged_in ) {
+  include("headers.php");
+  echo "<h3>Please log on for access to work requests</h3>\n";
+  include("footers.php");
+  exit;
+}
+
+require_once("maintenance-page.php");
+require_once("Request.class");
+
+$wr = new Request($request_id);
+if ( $wr->request_id == 0 ) {
+  unset( $request_id );
+  $edit = 1;
+  $title = ( intval($request_id) > 0 ? "Request Unavailable" : "New Request" );
+}
+else {
+  $title = "WR#$wr->request_id - $wr->brief";
+}
+$show = 0;
+$new = isset($edit) && intval($edit) && !isset($id);
+
+if ( $M != "LC" && $wr->AllowedTo("update") && isset($_POST['submit']) ) {
+  if ( $wr->Validate($wrf) ) {
+    $wr->Write($wrf);
+  }
+}
+
+  include("headers.php");
+
+echo "<p><a href=/wr.php?request_id=$request_id>Plain</a> &nbsp; ";
+echo "<a href=/wr.php?request_id=$request_id&edit=1>Editable</a></p>\n";
+echo $wr->Render();
+
+  include("footers.php");
+?>

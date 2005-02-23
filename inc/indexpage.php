ad7f5205113b8c64095f402dd881f75ef0aa4fc2
andrew <andrew>
2005-02-23 00:39:56 +0000

    Make the mucking around with the index page more generic.

--- inc/indexpage.php
+++ inc/indexpage.php
@@ -7,23 +7,15 @@ if ( $logged_on ) {
     include("indexclients.php");
   }
 }
+else if ( function_exists("local_index_not_logged_in") ) {
+  local_index_not_logged_in();
+}
 else { ?>
-<blockquote>
-<p><strong>
-Welcome to the technical support system of the Open Source Virtual Learning Environment project. For more information on the project please visit  <a href="http://www.ose.org.nz">www.ose.org.nz</a>. 
-</strong></p>
-</blockquote>
-<p>The goal of the project is to select, further develop and support open source e-learning software for deployment throughout New Zealand's education sector.</p>
-<p>
-Eduforge Support is delivered by <a href="http://catalyst.net.nz/products-moodle.htm">Catalyst IT Limited</a> , a trusted Moodle Partner and the core development team on the OSVLE project.
-To set-up Moodle hosting and support on the Education Cluster please read our project wikis:
-</p>
 
-<ul>
-<li><a href="http://eduforge.org/wiki/wiki/nzvle/wiki?pagename=VLESetup">Setup of your Virtual learning Environment</a></li>
-<li><a href="http://eduforge.org/wiki/wiki/nzvle/wiki?pagename=VLESupport">Support for your Virtual learning Environment</a></li>
-</ul>
-<p>Please e-mail <a href="mailto:<?php echo $admin_email; ?>"><?php echo $admin_email; ?></a> if you require further information.</p>
+<H4>For access to the <?php echo $system_name; ?> you should log on with
+the username and password that have been issued to you.</H4>
+
+<h4>If you would like to request access, please e-mail <?php echo $admin_email; ?>.</h4>
 
 <?php } ?>
 

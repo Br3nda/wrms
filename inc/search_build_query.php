bc62160a2e1b8fac7027a14b7070acf7acdc43ae
andrew <andrew>
2005-03-09 08:04:21 +0000

    Extend search to include request_notes table.

--- inc/search_build_query.php
+++ inc/search_build_query.php
@@ -76,7 +76,8 @@
 
     if ( "$search_for" != "" ) {
       $search_query .= " AND (brief ~* '$search_for' ";
-      $search_query .= " OR detailed ~* '$search_for' ) ";
+      $search_query .= " OR detailed ~* '$search_for' ";
+      $search_query .= " OR EXISTS(SELECT 1 FROM request_note WHERE request_id = request.request_id AND note_detail ~* '$search_for')) ";
     }
     if ( "$system_code" != "" )     $search_query .= " AND system_code='$system_code' ";
     if ( "$type_code" != "" )     $search_query .= " AND request_type=" . intval($type_code);

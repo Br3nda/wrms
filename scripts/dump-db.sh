d825df3ec9dd365330bcde50242ec11d28a92937
catalyst <catalyst>
2005-03-29 09:23:01 +0000

    Merge local changes with remote ones.

--- scripts/dump-db.sh
+++ scripts/dump-db.sh
@@ -6,6 +6,8 @@ if [ "$DBHOST" != "" ] ; then
   DBHOST="-i -U general -h ${DBHOST}"
 fi
 
+[ "${DEBUG}" != "" ] && set -o xtrace
+
 if [ ! -e ${DUMPDIR} ]
 then
    mkdir -p ${DUMPDIR}
@@ -19,16 +21,19 @@ fi
 echo "Dumping to directory: ${DUMPDIR}"
 
 echo "Dumping schema"
-pg_dump -s ${DBHOST} -n -d ${DATABASE} >${DUMPDIR}/schema-dump.sql
+pg_dump -s ${DBHOST} -Fp ${DATABASE} >${DUMPDIR}/schema-dump.sql
 
 for TABLE in `psql -qt -d $DATABASE -c "SELECT relname from pg_class where relkind != 'i' AND relname !~ '^pg_.*' AND relname !~ '^pga.*' AND relname !~ '.*_seq$';"` ; do
   echo "processing $TABLE"
   if [ "$TABLE" = "request_history" ]; then
-    pg_dump -a -n ${DBHOST} -D -d ${DATABASE} -t $TABLE >${DUMPDIR}/t-$TABLE.sql
+    pg_dump -a ${DBHOST} -Fp -D -t $TABLE ${DATABASE} >${DUMPDIR}/t-$TABLE.sql
   else
-    pg_dump -a -n ${DBHOST} ${DATABASE} -t $TABLE >${DUMPDIR}/t-$TABLE.sql
+    pg_dump -a ${DBHOST} -Fp -t $TABLE ${DATABASE} >${DUMPDIR}/t-$TABLE.sql
   fi
 done
 
 # Remove the request_words table which is built on load, if at all.
 rm ${DUMPDIR}/t-request_words.sql >>/dev/null 2>&1
+
+cd ${DUMPDIR}
+tar cvfz ~/wrms-tables.tgz t-*.sql

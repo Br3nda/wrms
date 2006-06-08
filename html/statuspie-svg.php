f85022e953c45df026c0cf725cf0516f055cea33
Andrew McMillan <debian@mcmillan.net.nz>
2006-06-08 18:09:10 +1200

    Encourage the linked segments to be more representative of the chart selections.

--- html/statuspie-svg.php
+++ html/statuspie-svg.php
@@ -19,21 +19,43 @@
 
   // First, we construct SQL with a two-column result set
   $sql = "SELECT status_lookup.lookup_desc, count(request.request_id) ";
+
+  // The second column of the SQL result is a URL to click through to.
+  // We santitise all the variables as we build this
   $sql .= ", '/wrsearch.php?style=stripped&format=brief' ";
   if ( isset($org_code) ) {
     $org_code = intval($org_code);
     $sql .= "|| '&org_code=$org_code' ";
   }
-  $sql .= "|| '&incstat['||last_status||']=1' AS url_link ";
-
-  $sql .= "FROM request JOIN request_status ON request.request_id=request_status.request_id AND status_code=last_status ";
-  $sql .= "JOIN lookup_code status_lookup ON status_lookup.source_table='request' AND status_lookup.source_field='status_code' AND status_lookup.lookup_code=last_status ";
   if ( isset($allocated_to) ) {
     $allocated_to = intval($allocated_to);
-    $sql .= "JOIN request_allocated ON request.request_id=request_allocated.request_id AND allocated_to_id = $allocated_to ";
+    $sql .= "|| '&allocated_to=$allocated_to' ";
   }
   if ( isset($interested_in) ) {
     $interested_in = intval($interested_in);
+    $sql .= "|| '&interested_in=$interested_in' ";
+  }
+  if ( isset($system_id) ) {
+    $system_id = intval($system_id);
+    $sql .= "|| '&system_id=$system_id' ";
+  }
+  if ( isset($request_type) ) {
+    $sql .= "|| '&request_type=".urlencode($request_type)."' ";
+    $request_type = qpg($request_type);
+  }
+  if ( isset($requested_by) ) {
+    $requested_by = intval($requested_by);
+    $sql .= "|| '&requested_by=$requested_by' ";
+  }
+  $sql .= "|| '&incstat['||last_status||']=1' AS url_link ";
+
+  // And the rest of the SQL is 'reasonably' straightforward...
+  $sql .= "FROM request JOIN request_status ON request.request_id=request_status.request_id AND status_code=last_status ";
+  $sql .= "JOIN lookup_code status_lookup ON status_lookup.source_table='request' AND status_lookup.source_field='status_code' AND status_lookup.lookup_code=last_status ";
+  if ( isset($allocated_to) ) {
+    $sql .= "JOIN request_allocated ON request.request_id=request_allocated.request_id AND allocated_to_id = $allocated_to ";
+  }
+  if ( isset($interested_in) ) {
     $sql .= "JOIN request_interested ON request.request_id=request_interested.request_id AND request_interested.user_no = $interested_in ";
   }
   if ( isset($org_code) ) {
@@ -41,18 +63,11 @@
   }
   $sql .= "WHERE ((last_status IN ('F', 'C') AND status_on BETWEEN ".qpg($from_date)." AND ".qpg($to_date).") ";
   $sql .= "OR last_status NOT IN ('F','C')) ";
-  if ( isset($system_id) ) {
-    $system_id = qpg($system_id);
-    $sql .= "AND request.system_id=$system_id ";
-  }
-  if ( isset($request_type) ) {
-    $request_type = qpg($request_type);
-    $sql .= "AND request.request_type=$request_type ";
-  }
-  if ( isset($requested_by) ) {
-    $requested_by = intval($requested_by);
-    $sql .= "AND requester_id = $requested_by ";
-  }
+
+  if ( isset($system_id) )      $sql .= "AND request.system_id=$system_id ";
+  if ( isset($request_type) )   $sql .= "AND request.request_type=$request_type ";
+  if ( isset($requested_by) )   $sql .= "AND requester_id = $requested_by ";
+
   $sql .= "GROUP BY status_lookup.lookup_desc, request.last_status ";
   $sql .= "ORDER BY 2 DESC;";
 

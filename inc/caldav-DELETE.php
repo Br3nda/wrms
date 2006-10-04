cddede3e79041de8a5e2065d7ada28fe570d0127
Andrew McMillan <debian@mcmillan.net.nz>
2006-10-05 00:45:14 +1300

    Beginnings of some limitation of effects on this.

--- inc/caldav-DELETE.php
+++ inc/caldav-DELETE.php
@@ -8,10 +8,37 @@ $delete_path = $_SERVER['PATH_INFO'];
 $etag_none_match = str_replace('"','',$_SERVER["HTTP_IF_NONE_MATCH"]);
 $etag_match = str_replace('"','',$_SERVER["HTTP_IF_MATCH"]);
 
-list( $junk, $user, $ts_id ) = preg_split( '#/#', $delete_path );
-$ts_id = intval($ts_id);
-$ts_url = sprintf("/%s/%d.ics", $session->username, $ts_id );
+$delete_user_no = $session->user_no;
+$delete_user_name = $session->username;
+if ( preg_match( "#^/([^/]+)/([0-9]+)(.*)\.ics$#", $delete_path, $matches ) ) {
+  $in_username = $matches[1];
+  $in_tsid = $matches[2];
+  $in_otherstuff = $matches[3];
+  $ts_id = intval($in_tsid);
+  if ( $in_otherstuff != '' && $in_otherstuff != '@'.$_SERVER['SERVER_NAME'] ) ) {
+    $ts_id = 0;
+    dbg_error_log('GET', "Looking very like this is not a timesheet: %s", $delete_path );
+  }
+}
 dbg_error_log('DELETE', "User: %s, TS_ID: %s, PATH: '%s'", $user, $ts_id, $delete_path );
+
+if ( $session->AllowedTo("Admin") ) {
+  $qry = new PgQuery( "SELECT user_no FROM usr WHERE username = ?;", $in_username );
+  if ( $qry->Exec("REPORT") && $row = $qry->Fetch() ) {
+    $delete_user_no = $row->user_no;
+    $delete_user_name = $in_username;
+  }
+}
+else {
+  if ( $in_username != $session->username ) {
+    header("HTTP/1.1 403 Access Denied");
+    header("Content-type: text/plain");
+    dbg_error_log( "DELETE", "Access denied: User: %d, Path: %s", $qry->rows, $session->user_no, $delete_path);
+    echo "Access Denied";
+    exit(0);
+  }
+}
+
 
 if ( ( !isset($etag_match) || $etag_match == '*' || $etag_match == '' )
        && (("/".$session->username."/$ts_id".".ics" == "$delete_path" )
@@ -22,7 +49,7 @@ if ( ( !isset($etag_match) || $etag_match == '*' || $etag_match == '' )
   $qry->Exec("PUT");
   if ( $qry->rows != 1 ) {
     header("HTTP/1.1 500 Infernal Server Error");
-    dbg_error_log("ERROR","Found %d rows matching request %d, timesheet %d for user %s(%d)", $request_id, $ts_id, $session->username, $session->user_no );
+    dbg_error_log("ERROR","Found %d rows matching request %d, timesheet %d for user %s(%d)", $request_id, $ts_id, $delete_user_name, $delete_user_no );
     exit(0);
   }
   elseif ( $qry->rows == 1 ) {

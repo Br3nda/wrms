bf971872a55456615ae4de23448c8b1d4b95cb3c
Andrew McMillan <andrew@dewey.db.catalyst.net.nz>
2006-12-14 10:10:03 +1300

    Allow the database name to be specified.

--- scripts/grant-perms.sh
+++ scripts/grant-perms.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-USER=$1
-PERMISSION="${2:-SELECT}"
+DBNAME=$1
+USER=$2
+PERMISSION="${3:-SELECT}"
 
 if [ "$USER" = "" ] ; then
   echo "Usage: $0 <username> [permissions]"
   exit
 fi
 
-TABLES="`psql wrms -qt -c \"select relname  from pg_class where relowner > 50 AND relkind in( 'r', 'S');\"`"
+TABLES="`psql "${DBNAME}" -qt -c \"select relname  from pg_class where relowner > 50 AND relkind in( 'r', 'S');\"`"
 
 for T in ${TABLES} ; do
-  psql wrms -c "grant ${PERMISSION} on ${T} to ${USER};"
+  psql "${DBNAME}" -c "grant ${PERMISSION} on ${T} to ${USER};"
 done

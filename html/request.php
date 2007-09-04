7b4f1b26216fd002cbdb992e087bf8d539a4e5bf
Andrew McMillan <debian@mcmillan.net.nz>
2007-09-04 20:02:59 +1200

    Remove dead code.

--- html/request.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $new_uri = str_replace('request.php','wr.php',$REQUEST_URI);
-
-  header("Location: $new_uri");
-?>
--- html/usr.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $new_uri = str_replace('usr.php','user.php',$REQUEST_URI);
-
-  header("Location: $new_uri");
-?>
